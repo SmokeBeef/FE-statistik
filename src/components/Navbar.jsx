b06b9b60fbc9730ae94d177cc2bcabe70871b641
M. Rasyid Ridho <rasyid.rdh05@gmail.com>
2023-06-01 07:31:41 +0700

    fix nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -47,14 +47,6 @@ export default function Navbar() {
       >
         Detail
       </a>
-      <a
-        href="/test"
-        className={`text-lg text-white py-5 text-center hover:text-2xl hover:text-gray-800 transition-all ${
-          window.location.pathname === "/test" ? "text-2xl text-gray-800 " : ""
-        }`}
-      >
-        Test
-      </a>
     </div>
   </div>
 </div>
